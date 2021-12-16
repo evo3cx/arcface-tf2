cc7dcb03cdf136df19e923ecdc2d7d2906314af4
reza <reza@graveltechnology.com>
2021-12-16 18:13:16 +0700

    update test model with GPU

--- test_model.py
+++ test_model.py
@@ -22,37 +22,40 @@ def main(_argv):
     logger = tf.get_logger()
     logger.disabled = True
     logger.setLevel(logging.FATAL)
-    set_memory_growth()
 
     cfg = load_yaml(FLAGS.cfg_path)
 
-    model = ArcFaceModel(size=cfg['input_size'],
-                         backbone_type=cfg['backbone_type'],
-                         training=False)
-    model.load_weights(cfg['pre_trained_model'])
-    print(model.summary())
+    gpus = tf.config.list_logical_devices('GPU')
+    strategy = tf.distribute.MirroredStrategy(gpus)
+    
+    with strategy.scope():
+        model = ArcFaceModel(size=cfg['input_size'],
+                            backbone_type=cfg['backbone_type'],
+                            training=False)
+        model.load_weights(cfg['pre_trained_model'])
+        print(model.summary())
 
-    print("[*] Loading LFW, AgeDB30 and CFP-FP...")
-    lfw, agedb_30, cfp_fp, lfw_issame, agedb_30_issame, cfp_fp_issame = \
-        get_val_data(cfg['test_dataset'])
+        print("[*] Loading LFW, AgeDB30 and CFP-FP...")
+        lfw, agedb_30, cfp_fp, lfw_issame, agedb_30_issame, cfp_fp_issame = \
+            get_val_data(cfg['test_dataset'])
 
-    print("[*] Perform Evaluation on LFW...")
-    acc_lfw, best_th = perform_val(
-        cfg['embd_shape'], cfg['batch_size'], model, lfw, lfw_issame,
-        is_ccrop=cfg['is_ccrop'])
-    print("    acc {:.4f}, th: {:.2f}".format(acc_lfw, best_th))
+        print("[*] Perform Evaluation on LFW...")
+        acc_lfw, best_th = perform_val(
+            cfg['embd_shape'], cfg['batch_size'], model, lfw, lfw_issame,
+            is_ccrop=cfg['is_ccrop'])
+        print("    acc {:.4f}, th: {:.2f}".format(acc_lfw, best_th))
 
-    print("[*] Perform Evaluation on AgeDB30...")
-    acc_agedb30, best_th = perform_val(
-        cfg['embd_shape'], cfg['batch_size'], model, agedb_30,
-        agedb_30_issame, is_ccrop=cfg['is_ccrop'])
-    print("    acc {:.4f}, th: {:.2f}".format(acc_agedb30, best_th))
+        print("[*] Perform Evaluation on AgeDB30...")
+        acc_agedb30, best_th = perform_val(
+            cfg['embd_shape'], cfg['batch_size'], model, agedb_30,
+            agedb_30_issame, is_ccrop=cfg['is_ccrop'])
+        print("    acc {:.4f}, th: {:.2f}".format(acc_agedb30, best_th))
 
-    print("[*] Perform Evaluation on CFP-FP...")
-    acc_cfp_fp, best_th = perform_val(
-        cfg['embd_shape'], cfg['batch_size'], model, cfp_fp, cfp_fp_issame,
-        is_ccrop=cfg['is_ccrop'])
-    print("    acc {:.4f}, th: {:.2f}".format(acc_cfp_fp, best_th))
+        print("[*] Perform Evaluation on CFP-FP...")
+        acc_cfp_fp, best_th = perform_val(
+            cfg['embd_shape'], cfg['batch_size'], model, cfp_fp, cfp_fp_issame,
+            is_ccrop=cfg['is_ccrop'])
+        print("    acc {:.4f}, th: {:.2f}".format(acc_cfp_fp, best_th))
 
 
 if __name__ == '__main__':
